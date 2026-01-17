a25946daf250223a38e427164d28a1b6a15f19ca
Hammad Ahmed <syedhammad283@gmail.com>
2026-01-18 00:43:10 +0500

    add file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hellow node")
+import fs from "fs"
+
+// console.log("hellow node")
+
+// fs.writeFileSync("tst.txt", "hey node", "utf-8") 
+
+const data = fs.readFileSync("./tst.txt", "utf-8");
+console.log(data)
+
+//   fs.mkdir('./view', (er) => {
+//       if (er) console.log(er);
+//  });
--- sum.js
+++ sum.js
@@ -2,4 +2,7 @@
 export function sum(y,z) {
   return y + z
 }
+
+
+
 console.log(sum(5, 5));
